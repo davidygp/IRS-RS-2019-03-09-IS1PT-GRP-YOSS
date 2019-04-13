a734c06547b4d018c61ce85cb9af1f88e5c8e3ad
Li Duo <liduosmu@gmail>
2019-04-13 15:36:29 +0800

    Commit Structure

---
+++ SystemCode/Scheduler/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'Scheduler'
+urlpatterns = [
+    path('', views.welcome, name='welcome'),
+    path('eligibility/', views.eligibility, name='eligibility'),
+    path('calendar/', views.calendar, name='calendar'),
+    path('file/', views.file, name='file'),
+
+    #path('spending/', views.spending, name='spending'),
+    #path('bank/', views.bank, name='bank'),
+    #path('end/', views.end, name='end')
+]
--- SystemCode/Scheduler/views.py
+++ SystemCode/Scheduler/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+debug = True
+
+def welcome(request):
+    # This is the first html page
+    return render(request, 'Recommender/welcome.html')
+
+def calendar(request):
+    # This is the first html page
+    return render(request, 'Recommender/calendar.html')
+
+def file(request):
+    # This is the first html page
+    return render(request, 'Recommender/file.html')
