1140019d1fcddb76edaecdc6a10880ba43045ad8
Your Name <you@example.com>
2019-04-17 20:25:39 +0800

    Get uploaded file into Django

--- SystemCode/Scheduler/views.py
+++ SystemCode/Scheduler/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+import os
 
 # Create your views here.
 
@@ -14,16 +15,19 @@ def welcome(request):
 def calendar(request):
     # This is the first html page    
 
-    return render(request, 'Scheduler/calendar.html')
-
-def file(request):
-    # This is the first html page
     if request.method == 'POST' and request.FILES['document']:
         uploaded_file = request.FILES['document']
         fs = FileSystemStorage()
         fs.save(uploaded_file.name, uploaded_file)
 
+    file_location = ''.join([fs.location, '/', uploaded_file.name])
+    print(file_location)
 
+    return render(request, 'Scheduler/calendar.html')
+
+def file(request):
+    # This is the first html page
+    
     return render(request, 'Scheduler/file.html')
 
 def file1(request):
